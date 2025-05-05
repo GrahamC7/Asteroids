df0a0d6e33891fdf8ab871b7d15c3fef8c3364a0
GrahamC7 <scocker@wgu.edu>
2025-05-05 17:56:32 -0400

    created initial files: main.py, constant.py, .gitignore, requirements.txt, fixed main.py structure

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import pygame
 from constants import *
 
 def main():
+    print("Starting Asteroids!")
+    print("Screen width: 1280")
+    print("Screen height: 720")
+
     pygame.init() # initialize pygame
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -13,14 +17,10 @@ def main():
 
         pygame.display.flip() # update the display
 
+        # event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-
-    print("Starting Asteroids!")
-    print("Screen width: 1280")
-    print("Screen height: 720")
 
 if __name__ == "__main__":
     main()
