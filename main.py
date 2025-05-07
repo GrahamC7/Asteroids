ae0893c63c8aead9e91c98f75b3e84a8b3de6c09
GrahamC7 <scocker@wgu.edu>
2025-05-07 13:39:52 -0400

    added moveability to the player

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
-    print("Starting Asteroids!")
-    print("Screen width: 1280")
-    print("Screen height: 720")
-
-    pygame.init() # initialize pygame
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    # create groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    #set containers for Player class
+    Player.containers = (updatable, drawable)
+
+    # create player after setting containers
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
-        screen.fill("black") # fill the screen with black color
-
-        pygame.display.flip() # update the display
-
-        # event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
+
+        screen.fill("black")
+        for entity in drawable:
+            entity.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
